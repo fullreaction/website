e0ff687479aef4d965b1143eaf4018f73a08020a
IndigoPendragon <56551165+IndigoPendragon@users.noreply.github.com>
2021-05-24 15:10:50 +0400

    sidenav

--- packages/frontend/src/components/app-docs/app-docs.tsx
+++ packages/frontend/src/components/app-docs/app-docs.tsx
@@ -9,7 +9,18 @@ export class AppDocs {
   render() {
     return (
       <div class="app-docs">
-        <p>documentation</p>
+        <div class="sidenav">
+        <a href="#getting-started">Getting Started</a>
+          <a href="#information">Information</a>
+          <a href="#services">Services</a>
+          <a href="#product-info">Product Info</a>
+          <a href="#company-info">Company Info</a>
+          <a href="#about">About</a>
+          <a href="#clients">Clients</a>
+
+        </div>
+
+        <div class="main"></div>
       </div>
     );
   }

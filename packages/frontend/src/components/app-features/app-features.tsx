10f8cb92875e203ce4e46cc3c4c18ce083afc95e
Ratiyoo <heraccle@gmail.com>
2021-05-27 15:58:02 +0400

    Commit
    Fixed problems with the features section, tweaked some things with the support section.

--- packages/frontend/src/components/app-features/app-features.tsx
+++ packages/frontend/src/components/app-features/app-features.tsx
@@ -13,20 +13,17 @@ export class AppFeatures {
         <div class="Features-Textboxes">
           <div class="Features-Textbox">
             <p>
-              One API to publish your content to <br></br>Facebook, Linkedin, Instagram,<br></br> Youtube and more One API to publish<br></br> your content to Facebook, Linkedin
-              <br></br> Instagram, Youtube and more
+              One API to publish your content to Facebook, Linkedin, Instagram, Youtube and more One API to publish your content to Facebook, Linkedin Instagram, Youtube and more
             </p>
           </div>
           <div class="Features-Textbox">
             <p>
-              One API to publish your content to <br></br>Facebook, Linkedin, Instagram,<br></br> Youtube and more One API to publish<br></br> your content to Facebook, Linkedin
-              <br></br> Instagram, Youtube and more
+              One API to publish your content to Facebook, Linkedin, Instagram, Youtube and more One API to publish your content to Facebook, Linkedin Instagram, Youtube and more
             </p>
           </div>
           <div class="Features-Textbox">
             <p>
-              One API to publish your content to <br></br>Facebook, Linkedin, Instagram,<br></br> Youtube and more One API to publish<br></br> your content to Facebook, Linkedin
-              <br></br> Instagram, Youtube and more
+              One API to publish your content to Facebook, Linkedin, Instagram, Youtube and more One API to publish your content to Facebook, Linkedin Instagram, Youtube and more
             </p>
           </div>
         </div>
--- packages/frontend/src/components/app-support/app-support.tsx
+++ packages/frontend/src/components/app-support/app-support.tsx
@@ -10,20 +10,22 @@ export class AppSupport {
       <div class="Support-Box">
         <h2 class="Support-Header"> Support </h2>
         <div class="Support-Text">
-          <p>
-            One API to publish your content to <br></br> Facebook, Linkedin, Instagram, Youtube and more
-          </p>
+          <p>One API to publish your content to Facebook, Linkedin, Instagram, Youtube and more</p>
         </div>
         <div class="Button-Box-Support">
-          <stencil-route-link url="/docs">
+          <a href="/docs" class="Support-Links">
             <button class="Support-Buttons">Documentation</button>
-          </stencil-route-link>
-          <button class="Support-Buttons">
-            <img src="../../assets/icon/Github.svg" class="Support-Icons"></img>Github
-          </button>
-          <button class="Support-Buttons">
-            <img src="../../assets/icon/Discord.svg" class="Support-Icons"></img>Discord
-          </button>
+          </a>
+          <a href="Github Link" class="Support-Links">
+            <button class="Support-Buttons">
+              <img src="../../assets/icon/Github.svg" class="Support-Icons"></img>Github
+            </button>
+          </a>
+          <a href="Discord Link" class="Support-Links">
+            <button class="Support-Buttons">
+              <img src="../../assets/icon/Discord.svg" class="Support-Icons"></img>Discord
+            </button>
+          </a>
         </div>
       </div>
     );
